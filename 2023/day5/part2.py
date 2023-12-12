274c4b347edd816774001406230583e8ee3c7905
Han Ye Htun <sloth30799@gmail.com>
2023-12-13 02:59:26 +0630

    day5

--- 2023/day5/part2.py
+++ 2023/day5/part2.py
@@ -1,77 +1,52 @@
-def reverse_map(maps, value):
-    for map in maps:
-        if value in range(map[0], map[0] + map[2]):
-            difference = value - map[0]
-            return map[1] + difference
+from typing import List
 
-    return value
+file = open("./input.txt").read().strip().split("\n\n")
+
+seeds = [int(x) for x in file[0].replace("seeds: ", "").split(" ")]
+ranges = [(seeds[i], seeds[i] + seeds[i + 1]) for i in range(0, len(seeds), 2)]
+
+# Map format: [[destination_range_start, source_range_start, range_length], ...]
+maps = [
+    [[int(y) for y in x.split(" ")] for x in file[i].splitlines()[1::]]
+    for i in range(1, 8)
+]
+
+# Reverse the maps
+maps = [[(end, start, range_length) for start, end, range_length in m] for m in maps][
+    ::-1
+]
 
 
-def get_seed(data, value):
-    for key in data.keys():
-        value = reverse_map(data[key], value)
-
-    return value
-
-
-def get_lowest_location():
-    with open('./example.txt') as file:
-        lines = [line.strip() for line in file.readlines()]
-
-    data = {}
-    current_key = None
-    current_map = []
-
-    for line in lines:
-        if ':' in line:
-            key, values = line.split(':')
-            current_key = key.strip().lower()
-            current_map = list(map(int, values.strip().split()))
-        elif line.strip():
-            current_map.append(list(map(int, line.strip().split())))
-        else:
-            data[current_key] = current_map
-
-    data[current_key] = current_map
-
-    seeds = data['seeds']
-    data.pop('seeds', None)
-
-    seeds_range = []
-    seeds_start = []
-    for index in range(0, len(seeds) - 1, 2):
-        start, length = seeds[index:index+2]
-        seeds_range.append(range(start, start+length))
-        seeds_start.append(start)
-
-    reversed_data = {}
-
-    for key in reversed(data.keys()):
-        reversed_data[key] = data[key]
-
-    location = 0
-    while True:
-        seed = get_seed(reversed_data, location)
-
-        if any(seed in s_range for s_range in seeds_range):
-            for l in range(0, int(location / 2)):
-                p_seed = get_seed(reversed_data, l)
-
-                if any(p_seed in s_range for s_range in seeds_range):
-                    print(location)
-                    return
-
-            for l in range(int(location / 2), location):
-                p_seed = get_seed(reversed_data, l)
-
-                if any(p_seed in s_range for s_range in seeds_range):
-                    print(location)
-                    return
+def x_to_y(step: int, m: List[List[int]]) -> int:
+    for destination_range_start, source_range_start, range_length in m:
+        if step >= source_range_start and step < source_range_start + range_length:
+            step = destination_range_start + (step - source_range_start)
             break
 
-        location += 100
+    return step
 
 
-get_lowest_location()
-# 37384987
-# 67108864
+def find_location(seed):
+    x = seed
+    for m in maps:
+        x = x_to_y(x, m)
+
+    return x
+
+
+def contains(seed):
+    return any(start <= seed < end for start, end in ranges)
+
+
+location = 0
+while True:
+    seed = find_location(location)
+    if contains(seed):
+        print(f"r: {location}")
+        break
+
+    location += 1
+
+    # Print progress
+    if location % 1_000_000 == 0:
+        print(location)
